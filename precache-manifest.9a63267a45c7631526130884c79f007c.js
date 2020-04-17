4aa161f9ef14db7a3332e72178a31e8adf370886
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2020-04-17 02:57:12 -0700

    Updates

---
+++ precache-manifest.9a63267a45c7631526130884c79f007c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c11bd2d41df8f1bb8ee9c343afd36979",
+    "url": "/web-sploosh-kaboom/index.html"
+  },
+  {
+    "revision": "7bbc9a3079bfbaf05401",
+    "url": "/web-sploosh-kaboom/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5fc7859ba5f4cc3681a8",
+    "url": "/web-sploosh-kaboom/static/js/2.2b968baa.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/web-sploosh-kaboom/static/js/2.2b968baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bbc9a3079bfbaf05401",
+    "url": "/web-sploosh-kaboom/static/js/main.fbaf8662.chunk.js"
+  },
+  {
+    "revision": "308f178fba52e4be9912",
+    "url": "/web-sploosh-kaboom/static/js/runtime-main.f6a4d017.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/splooshkaboom/precache-manifest.efe84e4e9d939ab01e2f8c2b7d1e4685.js"
+  "/web-sploosh-kaboom/precache-manifest.9a63267a45c7631526130884c79f007c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/splooshkaboom/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/web-sploosh-kaboom/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
