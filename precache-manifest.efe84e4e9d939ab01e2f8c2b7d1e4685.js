70c15e49438989f9a7e5a2e345f3df60871a089c
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2020-04-17 02:43:00 -0700

    Updates

---
+++ precache-manifest.efe84e4e9d939ab01e2f8c2b7d1e4685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbe428d6f39ba5b783ad23466e49d4f",
+    "url": "/splooshkaboom/index.html"
+  },
+  {
+    "revision": "e691b56871ee003cac34",
+    "url": "/splooshkaboom/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5fc7859ba5f4cc3681a8",
+    "url": "/splooshkaboom/static/js/2.2b968baa.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/splooshkaboom/static/js/2.2b968baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e691b56871ee003cac34",
+    "url": "/splooshkaboom/static/js/main.b8a65de2.chunk.js"
+  },
+  {
+    "revision": "f95487bab8aed45a4bdb",
+    "url": "/splooshkaboom/static/js/runtime-main.07e85008.js"
+  }
+]);
