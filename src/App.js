fc8fd0aee5d5c0c88f9d8f9bb3238f99e4f361fb
Amphitryon <17864580+Amphitryon0@users.noreply.github.com>
2023-05-22 22:40:03 -0500

    Increment version

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import init, {
 } from './wasm/sploosh_wasm.js';
 const interpolate = require('color-interpolate');
 
-const VERSION_STRING = 'v0.0.21';
+const VERSION_STRING = 'v0.0.22';
 
 var globalDB = null;
 const indexedDBreq = window.indexedDB.open('splooshkaboom', 1);
